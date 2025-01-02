e7b4ccd9a20bdfd0be31e4e1d97dad11697889e0
kwamen98 <derrickdadson14@gmail.com>
2025-01-02 11:30:16 +0000

    navigation configuration added

---
+++ navigation/appNavigation.js
@@ -0,0 +1,24 @@
+import React from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import HomeScreen from "../screens/HomeScreen";
+import { LogBox } from "react-native";
+
+const Stack = createNativeStackNavigator();
+
+// Suppress warnings
+LogBox.ignoreLogs(["Non-serializable values were found in the navigation state"]);
+
+export default function AppNavigation() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen
+          name="Home"
+          component={HomeScreen}
+          options={{ headerShown: false }}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
---
+++ screens/HomeScreen.js
@@ -0,0 +1,15 @@
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import { View, Text,SafeAreaView ,Image} from "react-native";
+
+export default function HomeScreen() {
+  return (
+  <SafeAreaView>
+    <View>
+        <Text className="color-green-400">
+            Testing Navigation
+        </Text>
+    </View>
+  </SafeAreaView>
+  );
+}
